b3b891e27697b5f0adb4c2f55197862a705124bf
vLongas <longas.valentin@gmail.com>
2025-08-18 21:13:46 -0300

    cambios en depto

--- domain/departamentos.py
+++ domain/departamentos.py
@@ -1,5 +1,6 @@
 #crear clases de departamento Atributos: id, ubicacion, ambientes, capacidad, estado, precio_noche.
 
+
 departamento = [
     {
         "id": 1,
@@ -23,7 +24,7 @@ def agregar_departamento(departamento, id, ubicacion, ambientes, capacidad, esta
         "precio_noche": precio_noche
     }
     departamento.append(nuevo_departamento)
-    return departamento
+    return departamento 
 
 
 #funcion que permita eliminar un departamento de la lista de departamentos
@@ -34,5 +35,46 @@ def eliminar_departamento(departamento, id):
             print(f"Departamento con id {id} eliminado")
             return departamento
     else:
+        print(f"Departamento con id {id} no encontrado")   
+        return "Departamento no encontrado"
+
+#funcion que permita actualizar un departamento de la lista de departamentos
+def actualizar_departamento(departamento, id, ubicacion, ambientes, capacidad, estado, precio_noche):
+    for deptos in departamento:
+        if deptos["id"] == id:
+            deptos["ubicacion"] = ubicacion
+            deptos["ambientes"] = ambientes
+            deptos["capacidad"] = capacidad
+            deptos["estado"] = estado
+            deptos["precio_noche"] = precio_noche
+            print(f"Departamento con id {id} actualizado")
+            return departamento
+    else:
         print(f"Departamento con id {id} no encontrado")
         return "Departamento no encontrado"
+    
+
+#funcion que permita remplazar un departamento de la lista de departamentos
+def reemplazar_departamento(departamento, id, ubicacion, ambientes, capacidad, estado, precio_noche):
+    for i, deptos in enumerate(departamento):
+        if deptos["id"] == id:
+            departamento[i] = {
+                "id": id,
+                "ubicacion": ubicacion,
+                "ambientes": ambientes,
+                "capacidad": capacidad,
+                "estado": estado,
+                "precio_noche": precio_noche
+            }
+            print(f"Departamento con id {id} reemplazado")
+            return departamento
+    else:
+        print(f"Departamento con id {id} no encontrado")
+        return "Departamento no encontrado"
+
+#funcion que permita listar todos los departamentos de la lista de departamentos
+def listar_departamentos(departamento):
+    for deptos in departamento:
+        print(f"ID: {deptos['id']}, Ubicación: {deptos['ubicacion']}, Ambientes: {deptos['ambientes']}, Capacidad: {deptos['capacidad']}, Estado: {deptos['estado']}, Precio por noche: {deptos['precio_noche']}")
+    return departamento
+
