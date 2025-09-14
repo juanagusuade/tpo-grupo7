7b001889f8f74bf9243a2503d02ebade5e6d8bf7
evelyncent <ecenturion@uade.edu.ar>
2025-09-14 19:47:41 -0300

    menu clientes terminado

--- ui/menu_clientes.py
+++ ui/menu_clientes.py
@@ -1,107 +1,111 @@
-import domain.reservas as reservas
-import domain.departamentos as departamentos
+import common.interfaz as interfaz
 import domain.clientes as clientes
 from common.constantes import *
-from common.interfaz import *
-from common.entrada_datos import *
-from domain.departamentos import listar_departamentos_activos
+import common.entrada_datos as entrada_datos
 
-OPCIONES_MENU = ["1. Agregar reserva.", "2. Listar reservas.", "3. Eliminar reserva.", "4. Volver al menu principal."]
-def menuReservas():
+OPCIONES_MENU_CLIENTES = [
+    "Agregar cliente",
+    "Listar clientes",
+    "Eliminar cliente",
+    "Modificar cliente",
+    "Volver al menu principal"]
+
+
+def accion_agregar_cliente():
+    """Función que se llama desde el menú para agregar un cliente"""
+    print("\n--- Agregar cliente ---")
+    nombre = entrada_datos.pedir_texto_alfabetico("Nombre")
+    apellido = entrada_datos.pedir_texto_alfabetico("Apellido")
+    dni = entrada_datos.pedir_dni()
+    telefono = entrada_datos.pedir_telefono()
+
+    if clientes.agregar_cliente(nombre, apellido, dni, telefono):
+        interfaz.mostrar_mensaje_exito("Cliente agregado correctamente.")
+        print()
+    else:
+        interfaz.mostrar_mensaje_error("Error: Ya existe un cliente con ese DNI.")
+        print()
+
+
+def accion_listar_clientes():
+    """Función que se llama desde el menú para listar clientes activos"""
+    interfaz.mostrar_lista_clientes(clientes.listar_clientes_activos())
+         
+
+
+def accion_baja_cliente(): 
+    """Función que se llama desde el menú para dar de baja un cliente"""
+    print("\n--- Dar de baja un cliente ---")
+
+
+    activos = clientes.listar_clientes_activos()
+    if not activos:
+        interfaz.mostrar_mensaje_error("No hay clientes activos para modificar.")
+        return
+
+    print("\n--- Lista de clientes activos ---")
+    for c in activos:
+        print(f"[{c[ID_CLIENTE]}] {c[NOMBRE_CLIENTE]} {c[APELLIDO_CLIENTE]} - DNI: {c[DNI_CLIENTE]} - Tel: {c[TELEFONO_CLIENTE]}")
+    #id_cliente = entrada_datos.pedir_rango_numerico("Ingrese ID del cliente a dar de baja", 10000, 99999)
+    id_cliente=entrada_datos.seleccionar_elemento_de_lista(clientes.lista_clientes_copia(), ID_CLIENTE, "Seleccione el ID del cliente a eliminar") 
+
+    if clientes.baja_logica_cliente(id_cliente):
+        interfaz.mostrar_mensaje_exito("Cliente dado de baja correctamente.")
+        print()
+    else:
+        interfaz.mostrar_mensaje_error("Cliente no encontrado o ya inactivo.")
+        
+        print()
+        
+def accion_modificar_cliente():
+    """Función que se llama desde el menú para modificar un cliente"""
+    activos = clientes.listar_clientes_activos()
+    if not activos:
+        interfaz.mostrar_mensaje_error("No hay clientes activos para modificar.")
+        return
+
+    print("\n--- Lista de clientes activos ---")
+    for c in activos:
+        print(f"[{c[ID_CLIENTE]}] {c[NOMBRE_CLIENTE]} {c[APELLIDO_CLIENTE]} - DNI: {c[DNI_CLIENTE]} - Tel: {c[TELEFONO_CLIENTE]}")
+    
+    id_modificado=entrada_datos.seleccionar_elemento_de_lista(clientes.lista_clientes_copia(), ID_CLIENTE, "Seleccione el ID del cliente a modificar")
+ 
+    nombre = entrada_datos.pedir_texto_alfabetico("Nombre")
+    apellido = entrada_datos.pedir_texto_alfabetico("Apellido")
+    dni = entrada_datos.pedir_dni()
+    telefono = entrada_datos.pedir_telefono()
+
+    if entrada_datos.dni_repetido(dni,id_modificado, clientes.lista_clientes_copia()):
+        interfaz.mostrar_mensaje_error("Error: Ya existe un cliente con ese DNI.")
+        return
+
+    if clientes.actualizar_cliente(id_modificado, nombre, apellido, dni, telefono):
+
+        interfaz.mostrar_mensaje_exito("Cliente modificado correctamente.")
+      
+    else:
+        interfaz.mostrar_mensaje_error("Error:Cliente no encontrado o ya inactivo.")
+        
+
+def menuClientes():
     menuActivo = True
-
     while menuActivo:
-        mostrar_separador()
-        mostrar_opciones_menu(OPCIONES_MENU, "Menu de Reservas")
+        interfaz.mostrar_separador()
+        interfaz.mostrar_opciones_menu(OPCIONES_MENU_CLIENTES, "Menu de Clientes")
+        opcion = entrada_datos.pedir_opcion_menu(len(OPCIONES_MENU_CLIENTES))
 
-        opcion = pedir_input_con_validacion(
-            "Seleccione una opción (1-4): ",
-            lambda x: x in ["1", "2", "3", "4"],
-            "Opción inválida. Por favor, seleccione una opción válida."
-        )
-
-        mostrar_separador()
+        interfaz.mostrar_separador()
 
         if opcion == "1":
-            # Seleccionar cliente valido
-            clientes_activos = clientes.listar_clientes_activos(None)
-            if clientes_activos == []:
-                mostrar_mensaje_error("No hay clientes activos registrados.")
-
-            mostrar_opciones_menu(
-                [f"ID: {cliente[ID_CLIENTE]} - {cliente[NOMBRE_CLIENTE]} {cliente[APELLIDO_CLIENTE]}" for cliente in clientes_activos],
-                "Clientes Activos"
-            )
-
-            id_cliente = pedir_input_con_validacion(
-                "Seleccione un cliente por su ID",
-                lambda valor: valor.isdigit() and int(valor) in [cliente[ID_CLIENTE] for cliente in clientes_activos],
-                "Debe ingresar un ID válido de la lista."
-            )
-            id_cliente = int(id_cliente)
-
-            # Select dates
-            fecha_ingreso = pedir_fecha("Fecha de Ingreso (dd/mm/yyyy): ")
-            fecha_egreso = pedir_fecha("Fecha de Egreso (dd/mm/yyyy): ")
-
-            # Select department
-            departamentos_disponibles = listar_departamentos_disponibles(fecha_ingreso, fecha_egreso)
-            if not departamentos_disponibles:
-                mostrar_mensaje_error("No hay departamentos disponibles para las fechas seleccionadas.")
-                continue
-
-            mostrar_opciones_menu([depto[0] for depto in departamentos_disponibles], "Departamentos Disponibles")
-
-            id_departamento = pedir_input_con_validacion(
-                "Seleccione un departamento por su ID",
-                lambda valor: valor.isdigit() and int(valor) in [depto[1] for depto in departamentos_disponibles],
-                "Debe ingresar un ID válido de la lista."
-            )
-            id_departamento = int(id_departamento)
-
-            print(f"Cliente seleccionado: {id_cliente}, Departamento seleccionado: {id_departamento}")
-
+            accion_agregar_cliente()
         elif opcion == "2":
-            if len(reservas.reservas) == 0:
-                print("No hay reservas registradas.")
-            else:
-                print("\nLista de Reservas:")
-                for reserva in reservas.reservas:
-                    print(f"ID Reserva: {reserva[0]}, ID Cliente: {reserva[1]}, ID Departamento: {reserva[2]}, "
-                          f"Fecha Ingreso: {reserva[3]}, Fecha Egreso: {reserva[4]}, Estado: {reserva[5]}")
-
+            accion_listar_clientes()
         elif opcion == "3":
+            accion_baja_cliente()
+        elif opcion == "4":
+            accion_modificar_cliente()
+        elif opcion == "5":
+            print("Volviendo al menú principal...")
             menuActivo = False
-            print("Volviendo al Menu Principal...")
-
-        else:
-            print("Opción inválida. Por favor, seleccione una opción válida.")
-
-def listar_departamentos_disponibles(fecha_ingreso, fecha_egreso):
-    departamentos_disponibles = []
-    departamentos_activos = listar_departamentos_activos()
-
-    for depto in departamentos_activos:
-        if depto[INDICE_ESTADO] == ESTADO_DISPONIBLE and reservas.verificar_disponibilidad_departamento(depto[ID_DEPARTAMENTO], fecha_ingreso, fecha_egreso):
-            depto_str = f"ID departamento: {depto[ID_DEPARTAMENTO]} - Ubicación: {depto[UBICACION_DEPARTAMENTO]}, Ambientes: {depto[AMBIENTES_DEPARTAMENTO]}, Capacidad: {depto[CAPACIDAD_DEPARTAMENTO]}, Precio/noche: {depto[PRECIO_DEPARTAMENTO]}"
-            departamentos_disponibles.append((depto_str, depto[ID_DEPARTAMENTO]))
-    return departamentos_disponibles
-
-def seleccionar_departamento(fecha_ingreso, fecha_egreso):
-    departamentos_disponibles = listar_departamentos_disponibles(fecha_ingreso, fecha_egreso)
-
-    if not departamentos_disponibles:
-        mostrar_mensaje_error("No hay departamentos disponibles para las fechas seleccionadas.")
-        return None
-
-    # Mostrar las opciones disponibles
-    mostrar_opciones_menu([depto[0] for depto in departamentos_disponibles], "Departamentos Disponibles")
-
-    # Validar el input del usuario
-    id_departamento = pedir_input_con_validacion(
-        "Seleccione un departamento por su ID",
-        lambda valor: valor.isdigit() and int(valor) in [depto[1] for depto in departamentos_disponibles],
-        "Debe ingresar un ID válido de la lista."
-    )
-
-    return int(id_departamento)
+        # Pausa entre operaciones
+        entrada_datos.pausar()    
