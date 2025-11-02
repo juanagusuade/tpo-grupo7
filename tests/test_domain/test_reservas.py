8f381cf62899763892aa9251ee3027daa5b359e7
juanaguss.facu <juanaarias@uade.edu.ar>
2025-11-02 19:31:46 -0300

    ejemplos de tests

--- tests/test_domain/test_reservas.py
+++ tests/test_domain/test_reservas.py
@@ -1,13 +1,32 @@
 from domain.reservas import comparar_fechas_string, hay_solapamiento_fechas
 
 
-def test_comparar_fechas_basico():
+def test_comparar_fechas():
     """Test simple: comparar dos fechas diferentes"""
-    resultado = comparar_fechas_string("01/01/2024", "15/01/2024")
-    assert resultado == -1
+    fechaChica = "01/01/2024"
+    fechaGrande = "15/01/2024"
+    resultadoEsperadoNegativo = -1
+    resultadoEsperadoPositivo = 1
+
+    resultadoNegativo = comparar_fechas_string(fechaChica, fechaGrande)
+    resultadoPositivo = comparar_fechas_string(fechaGrande, fechaChica)
+
+    assert resultadoNegativo == resultadoEsperadoNegativo
+    assert resultadoPositivo == resultadoEsperadoPositivo
 
 
 def test_solapamiento_mismo_dia():
     """Test simple: verificar que checkout/checkin el mismo dia NO solapa"""
-    resultado = hay_solapamiento_fechas("01/01/2025", "05/01/2025", "05/01/2025", "10/01/2025")
-    assert resultado == False
+    no_solapados = hay_solapamiento_fechas("01/01/2025", "05/01/2025", "05/01/2025", "10/01/2025")
+    solapados = hay_solapamiento_fechas("01/01/2025", "05/01/2025", "04/01/2025", "10/01/2025")
+
+    assert no_solapados == False
+    assert solapados == True
+
+
+# quiero probar listar_clientes_activos:
+#     primero hago una lista de clientes con distintos estados
+#     hago otra lista con los clientes ya filtrados
+#     llamo a la funcion de listar_clientes_activos con la lista grande
+#     asserteo que el resultado de la funcion es igual a la de los clientes filtrados
+
